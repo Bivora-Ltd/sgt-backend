9993c6a2c7e19ebea68c505b70156c6abc96f472
chimdykekamsi <ckamsi04@gmail.com>
2024-06-29 05:33:41 +0100

    fixing uploads

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/uploads', express.static('./uploads'));
+app.use('/uploads/images', express.static('./uploads/images'));
 app.use("/api/v1", Router);
 app.use(errorHandler);
 
