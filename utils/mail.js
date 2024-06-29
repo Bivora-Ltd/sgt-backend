3ae2f075f0b7609e214683b33a02697728a2b6f9
chimdykekamsi <ckamsi04@gmail.com>
2024-06-29 16:44:21 +0100

    updated the social link

--- utils/mail.js
+++ utils/mail.js
@@ -24,13 +24,21 @@ const sendEmail = async (to, subject, message) => {
         <p style="font-size: 12px; color: #777;">&copy; 2024 Street Got Talent. All rights reserved.</p>
       </div>
       <div style="text-align: center; margin-top: 20px;">
-        <a href="https://www.tiktok.com/@streetsgottalent_" target="_blank" style="margin-right: 20px;">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a9/TikTok_logo.svg" alt="TikTok" style="vertical-align: middle; width: 24px; height: 24px;" />
-          <span style="font-size: 16px; color: #333; text-decoration: none; vertical-align: middle;">@streetgottalent</span>
+        <a href="https://www.tiktok.com/@xpatainment?_t=8nbfITf0F3W&_r=1" target="_blank" style="margin-right: 20px;">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/6/69/TikTok_logo.svg" alt="TikTok" style="vertical-align: middle; width: 24px; height: 24px;" />
+          <span style="font-size: 16px; color: #333; text-decoration: none; vertical-align: middle;">@xpatainment</span>
         </a>
-        <a href="https://www.instagram.com/streetsgottalent_/" target="_blank">
+        <a href="https://www.instagram.com/xpatainment_?igsh=aW1qc2RtaGt3bnBo" target="_blank" style="margin-right: 20px;">
           <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram" style="vertical-align: middle; width: 24px; height: 24px;" />
-          <span style="font-size: 16px; color: #333; text-decoration: none; vertical-align: middle;">@streetgottalent</span>
+          <span style="font-size: 16px; color: #333; text-decoration: none; vertical-align: middle;">@xpatainment_</span>
+        </a>
+        <a href="https://www.facebook.com/xpataintment?mibextid=kFxxJD" target="_blank" style="margin-right: 20px;">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook" style="vertical-align: middle; width: 24px; height: 24px;" />
+          <span style="font-size: 16px; color: #333; text-decoration: none; vertical-align: middle;">@xpatainment</span>
+        </a>
+        <a href="https://www.youtube.com/@xpatainment" target="_blank">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/YouTube_icon_%282013-2017%29.png" alt="YouTube" style="vertical-align: middle; width: 24px; height: 24px;" />
+          <span style="font-size: 16px; color: #333; text-decoration: none; vertical-align: middle;">@xpatainment</span>
         </a>
       </div>
     </div>
