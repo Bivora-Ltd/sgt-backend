a9ba1c5b42e8e7b67108a361a1ef7fa3e0b8a61c
chimdykekamsi <ckamsi04@gmail.com>
2025-05-28 15:21:01 +0100

    made changes to config

--- utils/mail.js
+++ utils/mail.js
@@ -1,14 +1,14 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: 'mail.privateemail.com',
-  port: 587,
-  secure: false,
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 const sendEmail = async (to, subject, message) => {
@@ -48,14 +48,14 @@ const sendEmail = async (to, subject, message) => {
     from: '"Street Got Talent" <xpat@streetgottalent.com>',
     to,
     subject,
-    html: htmlContent
+    html: htmlContent,
   };
 
   try {
     await transporter.sendMail(mailOptions);
-    console.log('Email sent');
+    console.log("Email sent");
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error("Error sending email:", error);
     return false;
   }
 };
