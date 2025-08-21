b745582afdcc2890a80ccd7c4eb1afffb0019baa
kamsi <ckamsi04@gmail.com>
2025-08-21 20:01:31 +0100

    matching new UI

--- models/contestant.model.js
+++ models/contestant.model.js
@@ -1,54 +1,59 @@
 const Mongoose = require("mongoose");
 
-const contestantSchema = Mongoose.Schema({
-    name:{
+const contestantSchema = Mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    performanceType: {
+      type: String,
+      required: true,
+    },
+    socials: {
+      tiktok: {
         type: String,
-        required: true
-    },
-    imageUrl:{
+      },
+      instagram: {
         type: String,
-        required: true
+      },
     },
-    performanceType:{
-        type: String,
-        required: true
+    email: {
+      type: String,
+      required: true,
     },
-    socials:{
-        tiktok:{
-            type: String
-        },
-        instagram:{
-            type: String
-        }
+    votes: {
+      type: Number,
+      default: 0,
     },
-    email:{
-        type: String,
-        required: true
+    season: {
+      type: Mongoose.Schema.Types.ObjectId,
+      ref: "Season",
     },
-    votes:{
-        type: Number,
-        default: 0
-    },
-    season:{
-        type: Mongoose.Schema.Types.ObjectId,
-        ref: "Season"
-    },
-    group:{
-        type: String,
-        default: null
+    group: {
+      type: String,
+      default: null,
     },
     status: {
-        type: String,
-        default: "audition"
+      type: String,
+      default: "audition",
     },
-    phoneNumber:{
-        type: String,
-        required: true
-    }
-},
-    {
-        timestamps: true
-    }
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = Mongoose.model("Contestant", contestantSchema);
